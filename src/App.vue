1b4ce09e5d99001815951ddd3542eb3f37784de1
Diego Hernandez <dalvhst@gmail.com>
2025-06-05 21:16:04 -0500

    VueJS & Tailwind 4
    Start installer with Tailwind, and VueJS

---
+++ src/App.vue
@@ -0,0 +1,9 @@
+<template>
+  <div class="p-4">
+    <h1 class="text-center font-bold">VueJS With Tailwind CSS V4</h1>
+  </div>
+</template>
+
+<script lang="ts" setup></script>
+
+<style scoped></style>
---
+++ src/components/DefaultComponent.vue
@@ -0,0 +1,8 @@
+<!-- This component can be remove or rename -->
+<template>
+  <div></div>
+</template>
+
+<script lang="ts" setup></script>
+
+<style scoped></style>
